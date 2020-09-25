8c3cfd391c33f7686e1d97c8d8a619797b8bf05d
Nirakh Rastogi <nir16r@gmail.com>
2020-09-25 05:44:20 +0530

    Initial Commit

---
+++ src/main/java/com/spring/hikari/HikariPostgres/HikariPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.spring.hikari.HikariPostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikariPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikariPostgresApplication.class, args);
+	}
+
+}
